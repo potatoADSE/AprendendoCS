e7c4bde3d85b1f0161c21db2f7cb6658cdf7a712
potatoADSE <156465106+potatoADSE@users.noreply.github.com>
2024-02-08 12:00:54 -0300

    Update code.cs

--- code.cs
+++ code.cs
@@ -1,1 +1,15 @@
-# CalculatoInCS
+using System;
+
+  namespace Calculator Project;
+
+public class calculator {
+  static void Main(string[] args) {
+
+    Console.WriteLine(" Qual operação deseja efetuar? ");
+    Console.WriteLine(" 1 Adição ");
+    Console.WriteLine(" 2 Subtração ");
+    Console.WriteLine(" 3 Multiplicação ");
+    Console.WriteLine(" 4 Divisão ");
+    
+  }
+}
