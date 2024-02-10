ae003bbd8a0772cec422efffc21a81406e3b1e7b
potatoADSE <156465106+potatoADSE@users.noreply.github.com>
2024-02-10 17:06:04 -0300

    Create tentaiva do exercicio 2.cs

---
+++ tentaiva do exercicio 2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Data;
+
+  namespace mediacalculator;
+public class MediacalculatorCLASS {
+  static void Main(string[] args) {
+
+    Console.WriteLine("Escola a quantidade de numeros");
+    int[] numeros1 = new int[int.Parse(Console.ReadLine())];
+    Console.WriteLine(numeros1);
+
+  }
+}
