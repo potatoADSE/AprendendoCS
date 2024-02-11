5552b12e8e02e29029d869d9cd9c97eed11b31ec
Maias(batata/potato) <156465106+potatoADSE@users.noreply.github.com>
2024-02-11 20:51:54 -0300

    Update tentaiva do exercicio 2.cs

--- tentaiva do exercicio 2.cs
+++ tentaiva do exercicio 2.cs
@@ -3,14 +3,23 @@ using System.Linq;
 using System.Text;
 using System.Collections.Generic;
 using System.Data;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
 
-  namespace mediacalculator;
+namespace mediacalculator;
 public class MediacalculatorCLASS {
   static void Main(string[] args) {
 
-    Console.WriteLine("Escola a quantidade de numeros");
-    int[] numeros1 = new int[int.Parse(Console.ReadLine())];
-    Console.WriteLine(numeros1);
+    Console.WriteLine("Qual a quantidade de numeros deseja calcular a media?\nPor favor escolha de 3 a 10.");
 
+    int all = int.Parse(Console.ReadLine());
+    float[] all2 = new float[all];
+
+    foreach (float all3 in all2) {
+
+      Console.WriteLine("\nQuais o numeros?");
+      float numbers = float.Parse(Console.ReadLine());
+      
+    } 
   }
 }
